bfeb361c5205595f8ee434aeebd40816ff49fb8b
tapaswi-ctrl <tapaswee7@gmail.com>
2026-07-23 16:07:46 +0530

    Implement Selection Sort algorithm in C++
    This program implements the Selection Sort algorithm to sort an array of integers input by the user.

---
+++ SelectionSort.cpp
@@ -0,0 +1,53 @@
+//C++ program to perform the Selection Sort
+#include <iostream>
+using namespace std;
+
+// Function to perform Selection Sort
+void selectionSort(int arr[], int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        int minIndex = i;
+
+        // Find the index of the smallest element
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[j] < arr[minIndex])
+            {
+                minIndex = j;
+            }
+        }
+
+        // Swap the smallest element with the current element
+        int temp = arr[i];
+        arr[i] = arr[minIndex];
+        arr[minIndex] = temp;
+    }
+}
+
+int main()
+{
+    int n;
+
+    cout << "Enter the number of elements: ";
+    cin >> n;
+
+    int arr[n];
+
+    cout << "Enter the elements: ";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    // Call the function
+    selectionSort(arr, n);
+
+    cout << "Sorted array: ";
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
